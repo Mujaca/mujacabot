4017b6024ae0b4d3617c0f9380cdc99392edce47
Mujaca <mujaca0@gmail.com>
2023-04-21 22:59:57 +0200

    removed useless imports

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import { initialiseConsole } from './utils/console';
 import botManager from './manager/botManager';
 import commandManager from './manager/commandManager';
 import moduleManager from './manager/moduleManager';
-import { command } from "./classes/command";
-import { APIInteraction } from 'discord.js';
-import { WithIntrinsicProps } from '@discordjs/core';
 import { connectDatabase } from './manager/databaseManager';
 import { LewdOrNsFW } from './modules/LoN';
 
