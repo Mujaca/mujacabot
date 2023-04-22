a17223877cb5d80d9644929506750feacdda8423
Mujaca <mujaca0@gmail.com>
2023-04-22 14:06:18 +0200

    added module Registration mesasge

--- src/manager/moduleManager.ts
+++ src/manager/moduleManager.ts
@@ -3,6 +3,7 @@ import { Module } from "../classes/module";
 //TODO implement module manager
 
 function registerModule(name: string, module: Module) {
+    console.debug("Registering module: " + name);
 }
 
 export default {
