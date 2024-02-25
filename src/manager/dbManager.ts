1dd8df3457cef1e7167f2dae5a46511b7e2b360a
Mujaca <mujaca0@gmail.com>
2024-02-25 17:45:09 +0100

    database backup creator

--- src/manager/dbManager.ts
+++ src/manager/dbManager.ts
@@ -1,9 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import * as fs from "fs";
 const prisma = new PrismaClient();
 
 export async function connectDatabase(){
     await prisma.$connect();
     console.log("Connected to database");
+    backUpDatabase();
+}
+
+async function backUpDatabase(){
+    if(!fs.existsSync('./backup/')) fs.mkdirSync('./backup/');
+    const keys = Object.keys(prisma).filter(key => !key.startsWith('_') && !key.startsWith('$'));
+    for(let key of keys){
+        const data = await prisma[key].findMany();
+        fs.writeFileSync(`./backup/${key}.json`, JSON.stringify(data));
+    }
 }
 
 export default {
