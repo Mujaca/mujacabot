aac44ad4a9dd6b9bfd86fcbcb8a55d592cb70fbf
Mujaca <mujaca0@gmail.com>
2024-03-21 11:07:14 +0100

    first interact commands

---
+++ src/modules/trpg/commands/interact.ts
@@ -0,0 +1,46 @@
+import { ChatInputCommandInteraction } from "discord.js";
+import dbManager from "../../../manager/dbManager";
+import { getCharacter, removeFromCache } from "../manager/playerManager";
+
+export async function getGold(interaction: ChatInputCommandInteraction) {
+    const amount = interaction.options.getInteger("amount");
+    if(!amount) return await interaction.reply({ content: "Please provide an amount", ephemeral: true });
+
+    const character = await getCharacter(interaction.user.id);
+    if(!character) return await interaction.reply({ content: "You don't have a character yet", ephemeral: true });
+
+    await dbManager.db.rPGCharacter.update({
+        where: {
+            id: character.id,
+        },
+        data: {
+            gold: character.gold + amount,
+        },
+    });
+
+    removeFromCache(interaction.user.id);
+    interaction.reply({ content: `You now have ${character.gold + amount} gold`, ephemeral: true });
+}
+
+export async function removeGold(interaction: ChatInputCommandInteraction) {
+    const amount = interaction.options.getInteger("amount");
+    if(!amount) return await interaction.reply({ content: "Please provide an amount", ephemeral: true });
+
+    const character = await getCharacter(interaction.user.id);
+    if(!character) return await interaction.reply({ content: "You don't have a character yet", ephemeral: true });
+
+    let gold = character.gold - amount;
+    if(gold < 0) return await interaction.reply({ content: "You don't have enough gold", ephemeral: true });
+
+    await dbManager.db.rPGCharacter.update({
+        where: {
+            id: character.id,
+        },
+        data: {
+            gold: gold,
+        },
+    });
+
+    removeFromCache(interaction.user.id);
+    interaction.reply({ content: `You now have ${gold} gold`, ephemeral: true });
+}
