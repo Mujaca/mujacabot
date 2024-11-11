35052593cf8aba436f56c66d20cad5d217d65318
Jean-Michel Reupsch <reupsch@brunsdigital.de>
2024-11-11 16:07:52 +0100

    added music player manager

---
+++ src/modules/music/manager/playerManager.ts
@@ -0,0 +1,17 @@
+import { VoiceChannel } from "discord.js";
+import { musicplayer } from "../utils/player";
+
+const players:Map<string, musicplayer> = new Map();
+
+export function getPlayer(guildId: string):musicplayer {
+	let player = players.get(guildId);
+	if(!player) return null;
+
+	return player;
+}
+
+export function createPlayer(guildId: string, channel: VoiceChannel):musicplayer {
+	let player = new musicplayer(channel);
+	players.set(guildId, player);
+	return player;
+}
