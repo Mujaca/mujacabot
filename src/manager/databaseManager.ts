da31d8b5218271e1f94d4f363e70a251d8dd2af9
Jean-Michel Reupsch <reupsch@brunsdigital.de>
2024-11-13 14:44:27 +0100

    removed mongodb

--- src/manager/databaseManager.ts
+++ src/manager/databaseManager.ts
@@ -1,15 +1,5 @@
-import { MongoClient } from 'mongodb';
-import * as dotenv from "dotenv";
-dotenv.config();
-
-const client = new MongoClient(process.env.MONGO_URI);
-let db = client.db(process.env.DATABASE_NAME);
-
-export async function connectDatabase(){
-    await client.connect();
-    console.log("Connected to database");
-}
+const db = {}
 
 export default {
-    db
+	db
 }
